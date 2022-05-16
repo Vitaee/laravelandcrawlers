ff452babaa7873382fc811a6a9194f26c8b0e6ed
Vitaee <canilguu@gmail.com>
2022-05-16 14:33:30 +0300

    crawler updated

--- crawlers/firstcrawler.php
+++ crawlers/firstcrawler.php
@@ -1,7 +1,6 @@
 <?php
 
 require 'vendor/autoload.php';
-use Phpml\Clustering\DBSCAN;
 
 class crawler {
 
@@ -11,12 +10,12 @@ class crawler {
     function __construct()
     {
         $this->reqClient = new \Goutte\Client();
-        $this->prices = [];
+        $this->data = [ array("items") ];
         
     }
 
     function showData() {
-        print_r($this->prices)."\n";
+        echo json_encode($this->data, JSON_UNESCAPED_UNICODE)."\n";
         //echo json_encode(array('items' => $this->prices), JSON_UNESCAPED_UNICODE);
     }
 
@@ -30,26 +29,23 @@ class crawler {
     function scrape(): array|bool{
         $response = $this->start();
         
-        $response->filter('.row article div.product_price p.price_color')->each( function ($node) {
-            $this->prices[] = $node->text();
+
+        $response->filter('.row article')->each( function ($node){
+            $this->data[] =  array(
+                "price" =>$node->filter('div.product_price p.price_color')->text(), 
+                "title" => $node->filter('h3 a')->text(),
+                "image" => "https://books.toscrape.com/".$node->filter('div.image_container a img')->attr('src')
+            );
         });
 
-        return ( count($this->prices) >= 1 ) ? $this->prices : false;
+        
+
+
+        return ( count($this->data) >= 1 ) ? $this->data : false;
     }
 }
 
-//$first = new crawler();
-//$first->scrape();
-//$first->showData();
-
-//$sample1 = [[1, 1], [8, 7], [1, 2], [7, 8], [2, 1], [8, 9]];
-$samples = [ 'Label1' => [1, 1], 'Label2' => [8, 7], 'Label3' => [1, 2]];
-
-//$array1 = array( "clusters" => array( 0=> array( "center"=>array("long"=>2,"lang"=>3), "size"=>9 ), 1=>array( "center"=>array("long"=>21.2,"lang"=>9), "size"=>2 ) ) );
-
-//$kmeans = new KMeans(1);
-//$data = $kmeans->cluster($samples);
-//echo json_encode($data);
-
-
+$first = new crawler();
+$first->scrape();
+$first->showData();
 ?>
